8ff1a19e6933262f7bfd9cb5479d04b90d2c351e
norman <87526845@qq.com>
2016-08-18 09:06:21 +0800

    Initial Commit

---
+++ TextFiledCopyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextFiledCopyDemo
+//
+//  Created by gongxin on 16/8/18.
+//  Copyright © 2016年 gongxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextFiledCopyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextFiledCopyDemo
+//
+//  Created by gongxin on 16/8/18.
+//  Copyright © 2016年 gongxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
